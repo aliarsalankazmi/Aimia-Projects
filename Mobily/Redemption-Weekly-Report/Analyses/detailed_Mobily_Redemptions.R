7c62b43ebebd74ed7c95f4a5050cac95ab899715
Ali Arsalan <scac_1041@hotmail.com>
2015-11-20 08:49:46 +0300

    Create detailed_Mobily_Redemptions.R

---
+++ Mobily/Redemption-Weekly-Report/Analyses/detailed_Mobily_Redemptions.R
@@ -0,0 +1,57 @@
+df5 <- df1 %>%
+  filter(rdm_type == 'Mobily') %>%
+  select(redemption_we, msisdn, item_sub_category, pay_type_code, points_denomination) %>%
+  group_by(redemption_we, pay_type_code, item_sub_category, msisdn) %>%
+  summarise(total_redemptions_perCustomer = n(),
+            points_redeemed_perCustomer = sum(points_denomination)) %>%
+  group_by(redemption_we, pay_type_code, item_sub_category) %>%
+  summarise(total_redemptions = sum(total_redemptions_perCustomer),
+            unique_customers = n_distinct(msisdn),
+            total_points_redeemed = sum(points_redeemed_perCustomer),
+            avg_points_redeemed = mean(points_redeemed_perCustomer),
+            max_points_redeemed = max(points_redeemed_perCustomer),
+            min_points_redeemed = min(points_redeemed_perCustomer)) %>%
+  mutate(percent_total_redemptions = (total_redemptions/sum(total_redemptions))*100,
+         percent_points_redeemed = (total_points_redeemed/sum(total_points_redeemed))*100) %>%
+  arrange(redemption_we, pay_type_code, item_sub_category)
+
+g1 <- df5 %>%
+  ggplot(aes(x = redemption_we, y = item_sub_category)) +
+  geom_tile(aes(fill = percent_total_redemptions), colour = 'white') + scale_fill_gradient(low = 'white', high = 'steelblue') +
+  geom_text(aes(x = redemption_we, y = item_sub_category, label = paste0(round(percent_total_redemptions,0),'%')), alpha = .7, fontface = 'bold', family = 'Garamond', size = 3.5) +
+  facet_grid(pay_type_code ~ ., scales = 'free') + ggtitle('Percentage of Total Redemptions for Mobily Products') +  xlab('Week Ending Date') + ylab('') + myTheme + theme(panel.grid = element_blank(), axis.ticks = element_blank(), panel.border = element_blank(), legend.position = 'none')
+
+g2 <- df5 %>%
+  ggplot(aes(x = redemption_we, y = item_sub_category)) +
+  geom_tile(aes(fill = percent_points_redeemed), colour = 'white') + scale_fill_gradient(low = 'white', high = 'steelblue') +
+  geom_text(aes(x = redemption_we, y = item_sub_category, label = paste0(round(percent_points_redeemed,0),'%')), alpha = .7, fontface = 'bold', family = 'Garamond', size = 3.5) +
+  facet_grid(pay_type_code ~ ., scales = 'free') + ggtitle('Percentage of Total Points Redeemed for Mobily Products') +  xlab('Week Ending Date') + ylab('') + myTheme + theme(panel.grid = element_blank(), axis.ticks = element_blank(), panel.border = element_blank(), legend.position = 'none')
+
+
+print(g1)
+print(g2)
+
+
+df5.1 <- df5 %>%
+  ungroup() %>%
+  select(redemption_we, pay_type_code, item_sub_category, total_redemptions, unique_customers, 
+         total_points_redeemed,avg_points_redeemed, contains('percent'), contains('tidy')) %>%
+  arrange(pay_type_code, item_sub_category, redemption_we) %>%
+  group_by(pay_type_code, item_sub_category) %>%
+  mutate(percent_totRedemp_Change = round(percent_total_redemptions - lag(percent_total_redemptions)),
+         percent_totPts_Change = round(percent_points_redeemed - lag(percent_points_redeemed)),
+         totRedemp_Status = ifelse(percent_totRedemp_Change >= 0, 'increased', 'decreased'),
+         totPts_Status = ifelse(percent_totPts_Change >= 0, 'increased', 'decreased'),
+         redempNumTidy = tidyNum(total_redemptions),
+         ptsNumTidy = tidyNum(total_points_redeemed),
+         avgNumTidy = tidyNum(avg_points_redeemed)) %>%
+  ungroup() %>%
+  filter(redemption_we == max(redemption_we)) %>%
+  group_by(pay_type_code) %>%
+  arrange(desc(percent_total_redemptions))
+
+df5.1.Prepaid <- df5.1 %>%
+  filter(pay_type_code == 'Prepaid')
+
+df5.1.Postpaid <- df5.1 %>%
+  filter(pay_type_code == 'Postpaid')
